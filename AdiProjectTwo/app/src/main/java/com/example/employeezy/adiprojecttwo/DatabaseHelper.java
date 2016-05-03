30ca4784b71c6bee475350ebb59e9787aebac78b
Michael Marshall <hmikemarshall@gmail.com>
2016-05-02 19:29:11 -0700

    adds primitive database that doesn't really function to the application

---
+++ AdiProjectTwo/app/src/main/java/com/example/employeezy/adiprojecttwo/DatabaseHelper.java
@@ -0,0 +1,71 @@
+package com.example.employeezy.adiprojecttwo;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by EmployYeezy on 5/2/16.
+ */
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    public static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "Players.db";
+
+    public static final String SQL_CREATE_PLAYER_TABLE =
+            "CREATE TABLE players ( id INTEGER PRIMARY KEY, name TEXT, year TEXT )";
+
+    public static final String SQL_DROP_DROP_PLAYER_TABLE = "DROP TABLE IF EXISTS players";
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(SQL_CREATE_PLAYER_TABLE);
+    }
+
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL(SQL_DROP_DROP_PLAYER_TABLE);
+        onCreate(db);
+    }
+
+    public void insert(int id, String name, String year) {
+        SQLiteDatabase db = getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put("id", id);
+        values.put("name", name);
+        values.put("year", year);
+
+        db.insert("Players", null, values);
+    }
+
+    public Player getPlayer(int id){
+
+        SQLiteDatabase db = getReadableDatabase();
+        String[] projection = new String[] {"id", "name", "year"};
+        String selection = "id = ?";
+        String[] selectionArgs = new String[]{ String.valueOf(id)};
+        Cursor cursor = db.query("players", projection, selection, selectionArgs, null, null, null, null);
+        cursor.moveToFirst();
+        String name = cursor.getString( cursor.getColumnIndex("name"));
+        String year = cursor.getString( cursor.getColumnIndex("year"));
+
+        return new Player(id, name, year);
+    }
+
+    public void delete(int id){
+        SQLiteDatabase db = getWritableDatabase();
+        String selection = "id =?";
+        String [] selectionArgs = new String[] { String.valueOf(id)};
+        db.delete("players", selection, selectionArgs);
+    }
+
+
+}
+
+
+
--- AdiProjectTwo/app/src/main/java/com/example/employeezy/adiprojecttwo/MainActivity.java
+++ AdiProjectTwo/app/src/main/java/com/example/employeezy/adiprojecttwo/MainActivity.java
@@ -17,6 +17,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         handleIntent(getIntent());
+
+        DatabaseHelper db = new DatabaseHelper(this);
+
+        db.insert(1, "Jordan Clarkson", "2014");
+        db.insert(2, "D'Angelo Russel", "2015");
+
+        Player retrievedPlayer = db.getPlayer(2);
+
     }
 
     @Override
@@ -32,17 +40,16 @@ public class MainActivity extends AppCompatActivity {
         SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
         SearchView searchView = (SearchView) menu.findItem(R.id.search).getActionView();
         searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
-
         return true;
-
     }
 
     private void handleIntent(Intent intent) {
-
         if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
             String query = intent.getStringExtra(SearchManager.QUERY);
             Toast.makeText(MainActivity.this,"Searching for "+query,Toast.LENGTH_SHORT).show();
         }
     }
+
+
 
 }
