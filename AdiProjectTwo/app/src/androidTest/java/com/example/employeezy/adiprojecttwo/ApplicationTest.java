ee50c4d13b2ea123036b448328d9476622916462
Michael Marshall <hmikemarshall@gmail.com>
2016-05-06 10:51:11 -0700

    adds super clever database test method that is super original

--- AdiProjectTwo/app/src/androidTest/java/com/example/employeezy/adiprojecttwo/ApplicationTest.java
+++ AdiProjectTwo/app/src/androidTest/java/com/example/employeezy/adiprojecttwo/ApplicationTest.java
@@ -1,13 +1,33 @@
 package com.example.employeezy.adiprojecttwo;
 
 import android.app.Application;
+import android.content.Context;
 import android.test.ApplicationTestCase;
+import android.test.suitebuilder.annotation.SmallTest;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
 public class ApplicationTest extends ApplicationTestCase<Application> {
+
+    private Context context;
+
     public ApplicationTest() {
         super(Application.class);
     }
-}
+
+        @Override
+        protected void setUp() throws Exception {
+            super.setUp();
+            context = getSystemContext();
+        }
+
+        @SmallTest
+        public void test() {
+            int size = DatabaseHelper.getInstance(context).getRosterData().getCount();
+            assertEquals(12, size);
+        }
+
+
+
+    }
